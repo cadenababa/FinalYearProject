66361f3a0959d77785af5cf3c6e7bf0399ea440f
Mark Tennyson <46404058+marktennyson@users.noreply.github.com>
2021-06-20 22:55:23 +0530

    Update requiremtnts.txt file

--- test.py
+++ test.py
@@ -1,14 +1,30 @@
-import requests as r
-from base64 import b64encode
-import json
+# import requests as r
+# from base64 import b64encode
+# import json
 
-file = "student_data.xlsx"
-with open(file, "rb") as f:
-    enc = b64encode(f.read())
-enc = b"student_xlsx ," + enc
-payload = dict(
-    excel_enc_data = enc.decode('utf-8')
-)
-# payload = {'name':"aniket"}
-with r.post(url="http://127.0.0.1:8000/add-ca-marks-from-teacher", headers={'Content-Type':'application/json'}, data=json.dumps(payload)) as resp:
-    print (resp.text)
+# file = "student_data.xlsx"
+# with open(file, "rb") as f:
+#     enc = b64encode(f.read())
+# enc = b"student_xlsx ," + enc
+# payload = dict(
+#     excel_enc_data = enc.decode('utf-8')
+# )
+# # payload = {'name':"aniket"}
+# with r.post(url="http://127.0.0.1:8000/add-ca-marks-from-teacher", headers={'Content-Type':'application/json'}, data=json.dumps(payload)) as resp:
+#     print (resp.text)
+
+a = """
+ .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
+| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+| |      __      | || |  ________    | || |     _____    | || |  _________   | || |     _____    | |
+| |     /  \     | || | |_   ___ `.  | || |    |_   _|   | || | |  _   _  |  | || |    |_   _|   | |
+| |    / /\ \    | || |   | |   `. \ | || |      | |     | || | |_/ | | \_|  | || |      | |     | |
+| |   / ____ \   | || |   | |    | | | || |      | |     | || |     | |      | || |      | |     | |
+| | _/ /    \ \_ | || |  _| |___.' / | || |     _| |_    | || |    _| |_     | || |     _| |_    | |
+| ||____|  |____|| || | |________.'  | || |    |_____|   | || |   |_____|    | || |    |_____|   | |
+| |              | || |              | || |              | || |              | || |              | |
+| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+ '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
+"""
+
+print (a)
